66972cddafea7789a50a38f2d317c4de6874589a
siddharth ray <siddharthray@siddharths-MacBook-Pro.local>
2023-02-15 15:54:15 +0530

    initial4

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ const cors = require("cors");
 
 const session = require("express-session");
 const secretKeys = require("./config/config");
-
-let secrets;
-(async () => {
-    try {
-        secrets = await secretKeys();
-        console.log("keys ", res);
-    } catch (err) {
-        console.log("err");
-    }
-})();
-
 const passport = require("passport");
 
 const facebookStrategy = require("passport-facebook").Strategy;
@@ -45,24 +34,34 @@ passport.deserializeUser(function (user, cb) {
     cb(null, user);
 });
 
-passport.use(
-    new facebookStrategy(
-        {
-            clientID: secretKeys.clientID,
-            clientSecret: secretKeys.clientSecret,
-            callbackURL:
-                "https://ec2-43-205-240-221.ap-south-1.compute.amazonaws.com/auth/facebook/callback",
-        },
-        function (accessToken, refreshToken, profile, done) {
-            console.log("acess token ", accessToken);
-            console.log("refresh token ", refreshToken);
-            console.log("profile ", profile);
-            return done(null, profile);
-        }
-    )
-);
+let secrets;
+(async () => {
+    try {
+        secrets = JSON.parse(await secretKeys());
+        console.log("keys ", secrets);
 
-app.get("/flogin", addCustomHeader, passport.authenticate("facebook"));
+        passport.use(
+            new facebookStrategy(
+                {
+                    clientID: secrets.clientID,
+                    clientSecret: secrets.clientSecret,
+                    callbackURL:
+                        "https://ec2-43-205-240-221.ap-south-1.compute.amazonaws.com/auth/facebook/callback",
+                },
+                function (accessToken, refreshToken, profile, done) {
+                    console.log("acess token ", accessToken);
+                    console.log("refresh token ", refreshToken);
+                    console.log("profile ", profile);
+                    return done(null, profile);
+                }
+            )
+        );
+    } catch (err) {
+        console.log("err");
+    }
+})();
+
+app.get("/flogin", passport.authenticate("facebook"));
 
 app.get(
     "/auth/facebook/callback",
